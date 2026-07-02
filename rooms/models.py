859d5343c5bdab0f3350ac68b573e15ec72de591
UtkarshSingh-creater <utkarsh123000@gmail.com>
2026-07-02 12:17:47 +0530

    created rooms model

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from homes.models import Home
 
-# Create your models here.
+class Room(models.Model):
+    home=models.ForeignKey(
+        Home,
+        on_delete=models.CASCADE,
+        related_name="rooms"
+    )
+
+    name=models.CharField(max_length=100)
+    floor=models.PositiveIntegerField(default=1)
+
+
+    def __str__(self):
+        return self.name
