b373f36cbe345a34210167998036a91bda27bb04
UtkarshSingh-creater <utkarsh123000@gmail.com>
2026-07-02 23:16:27 +0530

    added templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            return redirect("dashboard")
+        else:
+            messages.error(request, "Invalid username or password")
+
+    return render(request, "accounts/login.html")
+
+
+def user_logout(request):
+    logout(request)
+    return redirect("login")
+
+def register(request):
+    return render(request, "accounts/register.html")
+
+
+def profile(request):
+    return render(request, "accounts/profile.html")
